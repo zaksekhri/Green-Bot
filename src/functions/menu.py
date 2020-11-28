76f6320c17716e6b1d61b05fbd5ab79b4c909c88
Zak Sekhri <ZakariaASekhri@gmail.com>
2020-11-27 19:06:25 -0500

    first menu completed

--- src/functions/menu.py
+++ src/functions/menu.py
@@ -2,13 +2,27 @@
 import discord
 from discord.ext import menus
 from discord.ext import commands
-
+    
 class menuManager(menus.Menu):
     def __init__(self, pages, form):
         super(menuManager, self).__init__()
         self.pages = pages
         self.current_page = 0
         self.form = form
+
+    async def update(self, payload):
+        """
+        Overwrites base menu update func to delete user reaction
+        """
+        if self._can_remove_reactions:
+            if payload.event_type == 'REACTION_ADD':
+                await self.bot.http.remove_reaction(
+                    payload.channel_id, payload.message_id,
+                    discord.Message._emoji_reaction(payload.emoji), payload.member.id
+                )
+            elif payload.event_type == 'REACTION_REMOVE':
+                return
+        await super().update(payload)
 
     async def change(self) -> None:
         """
